4090a73219e3fb185f8e962ffa9933a3804a8f10
agustinsaavedra2 <agustin.saavedra@usach.cl>
2023-11-03 20:59:45 -0300

    Función modificadora de flow y constructor de chatbot.

---
+++ CódigoFuente_20677670_SaavedraOlmos/TDAChatbot_20677670_SaavedraOlmos.pl
@@ -0,0 +1,3 @@
+chatbot(ChatbotID, Name, WelcomeMessage, StartFlowID, Flows,
+[ChatbotID, Name, WelcomeMessage, StartFlowID, ListFlows]):-
+    combine2(Flows, [], ListFlows).
--- CódigoFuente_20677670_SaavedraOlmos/TDAFlow_20677670_SaavedraOlmos.pl
+++ CódigoFuente_20677670_SaavedraOlmos/TDAFlow_20677670_SaavedraOlmos.pl
@@ -6,3 +6,19 @@ id_flow(ID, Flow):-
 
 options_flow(Options, Flow):-
     flow(_,_,Options,Flow).
+
+flowAddOption(Flow, NewOption, NewFlow):-
+    flow(ID, Name, _, Flow),
+    options_flow(Options, Flow),
+    combine1([NewOption | Options], [], FlowOptions),
+    flow(ID,Name,FlowOptions,NewFlow).
+
+combine2([],_,[]).
+
+combine2([H|T],List2,[H|L]):-
+    \+member(H,List2),
+    combine1(T,[H | List2],L).
+
+combine2([H|T],List2,L):-
+    member(H,List2),
+    combine1(T,List2,L).
