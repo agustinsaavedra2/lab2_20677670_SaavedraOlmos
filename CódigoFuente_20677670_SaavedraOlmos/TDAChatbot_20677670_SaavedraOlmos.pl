699a891b30ff3740efd49c9b121498346181f499
agustinsaavedra2 <agustin.saavedra@usach.cl>
2023-11-04 21:39:19 -0300

    Funciones selectoras del TDA Chatbot.

--- CódigoFuente_20677670_SaavedraOlmos/TDAChatbot_20677670_SaavedraOlmos.pl
+++ CódigoFuente_20677670_SaavedraOlmos/TDAChatbot_20677670_SaavedraOlmos.pl
@@ -1,3 +1,9 @@
 chatbot(ChatbotID, Name, WelcomeMessage, StartFlowID, Flows,
 [ChatbotID, Name, WelcomeMessage, StartFlowID, ListFlows]):-
     combine2(Flows, [], ListFlows).
+
+id_chatbot(ID, Chatbot):-
+    chatbot(ID,_,_,_,_,Chatbot).
+
+flows_chatbot(Flows, Chatbot):-
+    chatbot(_,_,_,_,Flows,Chatbot).
