446a4ae6e23de715d5f394c1ff9611f97486ae98
agustinsaavedra2 <agustin.saavedra@usach.cl>
2023-11-06 20:53:29 -0300

    Función modificadora TDA Chatbot y otras funciones.

--- CódigoFuente_20677670_SaavedraOlmos/TDAChatbot_20677670_SaavedraOlmos.pl
+++ CódigoFuente_20677670_SaavedraOlmos/TDAChatbot_20677670_SaavedraOlmos.pl
@@ -7,3 +7,19 @@ id_chatbot(ID, Chatbot):-
 
 flows_chatbot(Flows, Chatbot):-
     chatbot(_,_,_,_,Flows,Chatbot).
+
+chatbotAddFlow(Chatbot, Flow, NewChatbot):-
+    chatbot(ChatbotID, Name, WelcomeMessage, StartFlowID, _, Chatbot),
+    flows_chatbot(Flows, Chatbot),
+    append(Flows, [Flow], ListFlows),
+    chatbot(ChatbotID, Name, WelcomeMessage, StartFlowID, ListFlows, NewChatbot).
+
+combine3([],_,[]).
+
+combine3([H|T],List2,[H|L]):-
+    \+ member(H,List2),
+    combine3(T,List2,L).
+
+combine3([H|T],List2,L):-
+    member(H,List2),
+    combine3(T,List2,L).
